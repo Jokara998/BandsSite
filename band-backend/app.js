773fdaf7aa06815c52dd4d9a6625d069c458aea7
Stefan Jokic <stefanpelejokic@gmail.com>
2021-05-04 13:38:24 +0200

    Update app.js

--- band-backend/app.js
+++ band-backend/app.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 const cookieParser = require("cookie-parser")
 
 
-app.use(cors({credentials:true, origin:["http://localhost:8081", "https://bands-site.herokupapp.com"]}))
+app.use(cors({credentials:true, origin:["http://localhost:8081", "https://bands-site.herokuapp.com"]}))
 app.use(express.urlencoded({ extended: true , limit:'50mb' }));
 app.use(express.json({limit: '50mb', extended: true}));
 app.use(cookieParser());
